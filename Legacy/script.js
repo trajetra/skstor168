38a7560e0e26413ddbf8e3d054614d945ac5258b
trajetra <jetrakunkhmer546@gmail.com>
2024-12-15 19:13:34 +0700

    Add files via upload

---
+++ Legacy/script.js
@@ -0,0 +1,6 @@
+const signInBtnLink = document.querySelector('.signInBtn-link');
+const signUpBtnLink = document.querySelector('.signUpBtn-link');
+const wrapper = document.querySelector('.wrapper');
+signUpBtnLink.addEventListener('click', () => {
+    wrapper.classList.toggle('active');
+});
